54aed19dc43d1ff617f0b378be0e60e04ac9fdfb
Manuel Rodriguez <manrodri92@gmail.com>
2018-04-21 18:39:16 -0400

    Added interface inicial

---
+++ src/app/interfaces/inicial.ts
@@ -0,0 +1,18 @@
+export interface Inicial {
+codigo:string,
+razonSocial:string,
+representanteLegal:string,
+tipoDeEstacion:string,
+direccion:string,
+municipio:string,
+telefono:string,
+departamento:string,
+fechaInspeccion:Date,
+inclinacion:string,
+ubicacion:string,
+ubicacionLote:string,
+areaTerreno:string,
+colindancias:string,
+distanciaCentrosAglo:string,
+
+}
