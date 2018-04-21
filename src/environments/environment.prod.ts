ba1bed1b5f4f6c1d8de770ae4ffdd7f76f1de0f4
Manuel Rodriguez <manrodri92@gmail.com>
2018-04-20 20:30:18 -0400

    Added the ref to the database

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCmSbX27AUSLFhJD2MF0ze-DucnbN2fDsQ",
+    authDomain: "planillasanh.firebaseapp.com",
+    databaseURL: "https://planillasanh.firebaseio.com",
+    projectId: "planillasanh",
+    storageBucket: "planillasanh.appspot.com",
+    messagingSenderId: "312277597338"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCmSbX27AUSLFhJD2MF0ze-DucnbN2fDsQ",
+    authDomain: "planillasanh.firebaseapp.com",
+    databaseURL: "https://planillasanh.firebaseio.com",
+    projectId: "planillasanh",
+    storageBucket: "planillasanh.appspot.com",
+    messagingSenderId: "312277597338"
+  }
 };
