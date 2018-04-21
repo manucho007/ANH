30d9b73dd4a98473437f3037feae35221e5a0c32
Manuel Rodriguez <manrodri92@gmail.com>
2018-04-20 21:23:33 -0400

    Imported modules and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './user/profile/profile.component';
 
+import { CoreModule} from './core/core.module';
+
+import { AngularFireModule} from 'angularfire2';
+import { environment } from '../environments/environment';
+import { NavbarComponent } from './ui/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { AuthService } from './auth.service';
 import { FirestoreService } from './firestore.service';
 import { NotifyService } from './notify.service';
 
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule
   ],
   declarations: [],
   providers: [AuthService, FirestoreService, NotifyService]
