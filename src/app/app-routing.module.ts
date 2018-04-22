58709620bc05ea4dfa6f328aab439158c99d3e9f
Manuel Rodriguez <manrodri92@gmail.com>
2018-04-22 02:18:20 -0400

    Added routing to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,22 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from './user/login/login.component';
 import { HomeComponent } from './ui/home/home.component';
+import { MenuComponent } from './estacion-servicio/menu/menu.component';
+import { MenuEstacionServicioComponent } from './estacion-servicio/menu-estacion-servicio/menu-estacion-servicio.component';
+import { InspeccionInicialComponent } from './estacion-servicio/inspeccion-inicial/inspeccion-inicial.component';
 
 const routes: Routes = [
   {
   path: 'login',
   component: LoginComponent
+  },
+  {
+  path: 'menuEstacionServicio',
+  component: MenuEstacionServicioComponent
+  },
+  {
+  path: 'inspeccionInicial',
+  component: InspeccionInicialComponent
   },
   {
     path: '**',
@@ -21,7 +32,10 @@ const routes: Routes = [
 @NgModule({
   declarations:[
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent,
+    MenuEstacionServicioComponent,
+    InspeccionInicialComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
