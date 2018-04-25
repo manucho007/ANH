2b622c91d73b1e3b47bbc90f0c0e8c20980681f9
Manuel Rodriguez <manrodri92@gmail.com>
2018-04-25 03:14:49 -0400

    Added support for image uploads

--- src/app/estacion-servicio/inspeccion-inicial/inspeccion-inicial.component.ts
+++ src/app/estacion-servicio/inspeccion-inicial/inspeccion-inicial.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FirestoreService} from '../../core/firestore.service';
 import { InspeccionInicial } from '../../interfaces/inspeccion-inicial';
+
+import { FirestoreService} from '../../core/firestore.service';
+
+import { Observable} from 'rxjs/Observable';
+import {AngularFireStorage,AngularFireUploadTask } from 'angularfire2/storage';
+import { tap } from 'rxjs/operators';
+
+let pathEste, pathOeste, pathNorte, pathSur;
 @Component({
   selector: 'app-inspeccion-inicial',
   templateUrl: './inspeccion-inicial.component.html',
@@ -26,22 +33,128 @@ inspeccion:InspeccionInicial={
   calzada:"",
   flujoVehicular:"",
   distanciaEstacionProx:"",
+  estacionMasProx:"",
   electricidad:true,
   aguaPotable:true,
   telefono:true,
   alcantarillado:true,
   observaciones:"",
   nombreRepresentante:"",
-  ci:"",
-  nombreInspector:""
+  ciRepresentante:"",
+  nombreInspector:"",
+  fotoEste:"",
+  fotoOeste:"",
+  fotoNorte:"",
+  fotoSur:"",
 };
-  constructor(private db:FirestoreService) { }
+// main task
+task:AngularFireUploadTask;
+// Progress monitoring
+percentageOeste:Observable<number>;
+percentageEste:Observable<number>;
+percentageNorte:Observable<number>;
+percentageSur:Observable<number>;
+
+// Download URL
+downloadURLOeste: Observable<string>;
+downloadURLEste: Observable<string>;
+downloadURLNorte: Observable<string>;
+downloadURLSur: Observable<string>;
+
+  constructor(private db:FirestoreService, private storage:AngularFireStorage) { }
 
   ngOnInit() {
   }
+
+  startUploadOeste(event: FileList) {
+    // The File object
+    const file = event.item(0)
+    // Client-side validation example
+    if (file.type.split('/')[0] !== 'image') {
+      console.error('Formato no soportado ')
+      return;
+    }
+    // The storage path
+    pathOeste = `imagenes/${new Date().getTime()}_${file.name}`;
+    // Totally optional metadata
+    const customMetadata = { info: 'Foto de Oeste' };
+    // The main task
+    this.task = this.storage.upload(pathOeste, file, { customMetadata })
+    // The file's download URL
+    this.downloadURLOeste = this.task.downloadURL();
+    console.log(pathOeste)
+    // Progress monitoring
+    this.percentageOeste = this.task.percentageChanges();
+  }
+
+  startUploadEste(event: FileList) {
+    // The File object
+    const file = event.item(0)
+    // Client-side validation example
+    if (file.type.split('/')[0] !== 'image') {
+      console.error('Formato no soportado ')
+      return;
+    }
+    // The storage path
+    pathEste = `imagenes/${new Date().getTime()}_${file.name}`;
+    // Totally optional metadata
+    const customMetadata = { info: 'Foto del Este' };
+    // The main task
+    this.task = this.storage.upload(pathEste, file, { customMetadata })
+    // The file's download URL
+    this.downloadURLEste = this.task.downloadURL();
+    console.log(pathEste)
+    // Progress monitoring
+    this.percentageEste = this.task.percentageChanges();
+  }
+
+  startUploadNorte(event: FileList) {
+    // The File object
+    const file = event.item(0)
+    // Client-side validation example
+    if (file.type.split('/')[0] !== 'image') {
+      console.error('Formato no soportado ')
+      return;
+    }
+    // The storage path
+    pathNorte = `imagenes/${new Date().getTime()}_${file.name}`;
+    // Totally optional metadata
+    const customMetadata = { info: 'Foto del Norte' };
+    // The main task
+    this.task = this.storage.upload(pathNorte, file, { customMetadata })
+    // The file's download URL
+    this.downloadURLNorte = this.task.downloadURL();
+    console.log(pathNorte)
+    // Progress monitoring
+    this.percentageNorte = this.task.percentageChanges();
+  }
+
+  startUploadSur(event: FileList) {
+    // The File object
+    const file = event.item(0)
+    // Client-side validation example
+    if (file.type.split('/')[0] !== 'image') {
+      console.error('Formato no soportado ')
+      return;
+    }
+    // The storage path
+    pathSur = `imagenes/${new Date().getTime()}_${file.name}`;
+    // Totally optional metadata
+    const customMetadata = { info: 'Foto del Sur' };
+    // The main task
+    this.task = this.storage.upload(pathSur, file, { customMetadata })
+    // The file's download URL
+    this.downloadURLSur = this.task.downloadURL();
+    console.log(pathSur)
+    // Progress monitoring
+    this.percentageSur = this.task.percentageChanges();
+  }
   saveInspeccion(){
-    this.inspeccion.ubicacion;
-    this.inspeccion.ubicacionLote;
+    this.inspeccion.fotoOeste=pathOeste|| '';
+    this.inspeccion.fotoEste=pathEste|| '';
+    this.inspeccion.fotoNorte=pathNorte|| '';
+    this.inspeccion.fotoSur=pathSur|| '';
+    confirm("¿Desea ingresar los datos?");
     console.log(this.inspeccion);
     this.db.add('iniciales',this.inspeccion);
   }
