0f20f6a9143131a7ddf3ae42ce09be061ed3dbf1
Manuel Rodriguez <manrodri92@gmail.com>
2018-04-26 03:02:39 -0400

    Imported Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from './core/auth.service';
+import { NotifyService } from './core/notify.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
