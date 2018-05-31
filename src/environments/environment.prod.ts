110d7e5c776f59a93906c9b182d9b7af50180c07
Manuel Rodriguez <manrodri92@gmail.com>
2018-05-31 00:03:15 -0400

    Comment env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
-export const environment = {
-  production: true,
-  firebase:{
-    apiKey: "AIzaSyCmSbX27AUSLFhJD2MF0ze-DucnbN2fDsQ",
-    authDomain: "planillasanh.firebaseapp.com",
-    databaseURL: "https://planillasanh.firebaseio.com",
-    projectId: "planillasanh",
-    storageBucket: "planillasanh.appspot.com",
-    messagingSenderId: "312277597338"
-  }
-};
+// export const environment = {
+//   production: true,
+//   firebase:{
+//     apiKey: "AIzaSyCmSbX27AUSLFhJD2MF0ze-DucnbN2fDsQ",
+//     authDomain: "planillasanh.firebaseapp.com",
+//     databaseURL: "https://planillasanh.firebaseio.com",
+//     projectId: "planillasanh",
+//     storageBucket: "planillasanh.appspot.com",
+//     messagingSenderId: "312277597338"
+//   }
+// };
